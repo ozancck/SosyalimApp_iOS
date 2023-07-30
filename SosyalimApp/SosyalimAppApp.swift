f2cc80d7b78c44a3078416f975acbd47e7523c9e
U200709077 <134602322+ozancicekisteportal@users.noreply.github.com>
2023-07-30 20:36:29 +0300

    Initial Commit

---
+++ SosyalimApp/SosyalimAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SosyalimAppApp.swift
+//  SosyalimApp
+//
+//  Created by Ozan Çiçek on 30.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SosyalimAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
